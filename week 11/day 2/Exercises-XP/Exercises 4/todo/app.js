40cae3bcf0346e6f45d98a9eb3caa7b8278e6909
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-10-15 22:15:06 +0300

    update

--- week 11/day 2/Exercises-XP/Exercises 4/todo/app.js
+++ week 11/day 2/Exercises-XP/Exercises 4/todo/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = 3000;
 
 
-app.use(express.static('htt'))
+app.use(express.static('./public'))
 // Static Middleware
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -27,17 +27,3 @@ app.listen(PORT, function (err) {
 
 
 
-
-// const e = require('express')
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.use("/", express.static +"/index.html")
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
